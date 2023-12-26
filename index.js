b5465724fcd962008e4dd95d4459aca7d6256579
Muhamad Muslih <muhamadmuslih720@gmail.com>
2023-12-26 16:29:50 +0700

    fix cors

--- index.js
+++ index.js
@@ -4,19 +4,9 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 
 const app= express()
+app.use(cors())
+app.use(express.json({limit : "10mb"}))
 
-// Konfigurasi CORS
-const corsOptions = {
-    origin: 'http://localhost:3000', // Sesuaikan dengan alamat frontend Anda
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Izinkan penggunaan kredensial seperti cookies
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-};
-  
-app.use(cors(corsOptions));
-app.use(express.json({ limit: "10mb" }));
-  
 const PORT = process.env.PORT || 8080
 
 // koneksi mongo
