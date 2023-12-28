02ff7f356889c1fe43a90345c351a50d5b3bad36
Muhamad Muslih <muhamadmuslih720@gmail.com>
2023-12-28 21:41:18 +0700

    final cooors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 
 app.use(express.json({ limit: "10mb" }));
